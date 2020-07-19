42b0bd1b624ee73db0fefdfa5bd02130bae23ae0
Rodrigo Gularte Merida <gularter@mskcc.org>
2020-07-19 15:40:58 -0400

    rebuilding functions to convert to binary data

--- R/binary.cnr.R
+++ R/binary.cnr.R
@@ -14,13 +14,45 @@
 #'
 #' data(cnr)
 #'
-#' Z <- binary.cnr(cnr$genes[, c("CDK4", "MDM2")])
+#' Z <- binary.X(cnr$genes[, c("CDK4", "MDM2")])
 #' 
 #' 
 #' @export
-binary.cnr <- function(X) {
+binary.X <- function(X) {
     Z <- X
     Z[Z == 2] <- 0
     Z[Z != 0] <- 1
     Z
+} ## binary.X
+
+#' build binary CNR from integer copy number data
+#'
+#' This function builds a binary, incidence, matrix from the cnr$X.  It was designed as a helper function to generate the input for infSCITE.
+#'
+#' Because infSCITE was developed for mutation data, this function creates a precense/absence matrix of the data
+#' 
+#' By default, anything not diploid is 1.
+#'
+#'
+#' @param cnr a copy number matrix to convert to an incidence matrix
+#'
+#' 
+#' @examples
+#'
+#' data(cnr)
+#'
+#' Z <- binary.cnr(cnr)
+#' 
+#' 
+#' @export
+binary.cnr <- function(cnr) {
+
+    Z <- cnr[["X"]]
+    Z[Z == 2] <- 0
+    Z[Z != 0] <- 1
+    
+    cnr[["Z"]] <- Z
+    
+    return(cnr)
+    
 } ## binary.cnr
