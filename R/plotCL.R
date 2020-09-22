8b6ea6863980993d92fd6347914854bb80f2bfbd
Rodrigo Gularte Merida <gularter@mskcc.org>
2020-09-22 15:12:21 -0400

    debugging, and adding new functions

---
+++ R/plotCL.R
@@ -0,0 +1,23 @@
+#' plot cluster optimization
+#'
+#' @param optC output of optClust()
+#' 
+#' @param ... additional arguments passed to plot and points
+#'
+#' @examples
+#'
+#' data(cnr)
+#'
+#' cnr <- phyloCNR(cnr)
+#'
+#' ( mopc <- optClust(cnr, opt.range = seq(0.01, 0.2, by = 0.005)) )
+#'
+#' plotCL(optC = mopc)
+#' 
+#' @export
+plotCL <- function(optC, type = "b", ...) {
+    plot(rownames(optC), optC[,1], pch = 1, ylim = c(0, max(optC)),
+         type = type, ...)
+    points(rownames(optC), optC[,2], pch = 2, type = type,  ...)
+    legend("topright", legend = c("One-cell", "Multi-cell"), pch = 1:2)
+}
