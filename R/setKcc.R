afc9a26fc6238cb1eb29ee9ad6c86021f2b8b816
Rodrigo Gularte Mérida <gularter@mskcc.org>
2020-10-27 10:33:18 -0400

    Update setKcc.R

--- R/setKcc.R
+++ R/setKcc.R
@@ -13,12 +13,14 @@
 #' 
 #' @export
 setKcc <- function(cnr, K = 5) {
-    
+
     cc_membership <- as.data.frame(cnr[["ccp"]][[K]]$consensusClass)
     colnames(cc_membership) <- paste0("ccK", K)
 
     cnr <- addPheno(cnr, df = cc_membership, by.x = "cellID", by.y = 0,
                     sort = FALSE)
+    cnr[["optK"]] <- K
+    
     return(cnr)
 }
 
